78afe9be26138cbfa57bd8df5f847feca3031144
Your Name <you@example.com>
2021-11-21 11:14:23 +0700

    Add the lock test

---
+++ mag_test.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+relay_pin =18
+GPIO.setup(relay_pin, GPIO.OUT)
+
+init_button_pin = 24
+exit_button_pin = 25
+GPIO.setup(init_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(exit_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+GPIO.output(relay_pin, 0)
+
+if(GPIO.input(init_button_pin)==0):
+    GPIO.output(relay_pin, 1)
+if(GPIO.input(init_button_pin)==0):
+    GPIO.output(relay_pin, 0)
